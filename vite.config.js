cc3ec491cbc73af2cfad4b482a35e7b3a2cbefcd
adieptr <adie6809@gmail.com>
2025-01-26 18:57:12 +0700

    fix bug in wind.blade.php

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        tailwindcss(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'public/css/main.css'],
             refresh: true,
         }),
-
     ],
 });
